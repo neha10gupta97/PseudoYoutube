b8ba4077444cfb57f375a58a1bcf8051dbdf3818
Neha Gupta <neha10gupta97@gmail.com>
2018-04-01 23:53:25 +0530

    search_bar started

---
+++ src/components/search_bar.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+class SearchBar extends Component {
+  render(){
+    return <input/>;
+  }
+}
+// const SearchBar = () => {  Function compoenent
+//     return <input/>
+// };
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const API_KEY = AIzaSyDMzNRBenY3NZRCLpkLDbuRyfRsjTJcvUE;
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDMzNRBenY3NZRCLpkLDbuRyfRsjTJcvUE';
 
 const App=function(){
-    return <div>hi</div>;
+    return (
+    <div>
+        <SearchBar/>
+    </div>
+    );
 }
 
 ReactDOM.render(<App/>,document.querySelector('.container'));
