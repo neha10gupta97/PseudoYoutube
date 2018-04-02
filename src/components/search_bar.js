b00681cb46510999f166fee906e12b41ea72edbf
Neha Gupta <neha10gupta97@gmail.com>
2018-04-03 00:15:33 +0530

    search_bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,25 @@
 import React,{Component} from 'react';
 
 class SearchBar extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {term:''};
+  }
+
   render(){
-    return <input/>;
+    return (
+      <div>
+        <input
+          value={this.state.term}
+        onChange={event => this.setState({term: event.target.value })}/>
+      </div>
+    );
+  }
+
+  onInputChange(event){
+    console.log(event.target.value);
   }
 }
 // const SearchBar = () => {  Function compoenent
