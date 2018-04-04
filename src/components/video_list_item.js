a4aee8cf0a40ab16f2cdbdca0d816e43dcc2cc6c
Neha Gupta <neha10gupta97@gmail.com>
2018-04-04 23:32:37 +0530

    video_detail started

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,8 +2,21 @@ import React from 'react';
 
 const VideoListItem = ({video}) => {
   // const video = props.video;
+  const imageUrl = video.snippet.thumbnails.default.url;
   return(
-    <li>Video</li>
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl}/>
+        </div>
+
+        <div className="media-left">
+          <div className="media-heading">
+            {video.snippet.title}
+          </div>
+        </div>
+      </div>
+    </li>
   );
 }
 
