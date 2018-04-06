ea071702a35d1d2cf20e125334d3d839772e7300
Neha Gupta <neha.gupta@voylla.com>
2018-04-06 18:40:49 +0530

    video detail pg

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const VideoDetail = (video) => {
-  const videoId = vodeo.id.videoId;
+const VideoDetail = ({video}) => {
+  if(!video){
+    return <div>Loading...</div>
+  }
+  const videoId = video.id.videoId;
+  const url = `htpps://www.youtube.com/embed/${videoId}`;
+
   return(
     <div className="video-detail col-md-8">
       <div className="embed-responsive embed-responsive-16by9">
-        <iframe className="embed-responsive-item"/>
+        <iframe className="embed-responsive-item" src={url}></iframe>
       </div>
 
       <div className="details">
